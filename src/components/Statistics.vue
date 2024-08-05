cea48ecef66bf20153313629133146f5d9d164b7
choke <47597850+choke-dev@users.noreply.github.com>
2024-08-05 13:59:32 +0800

    add statistics

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,5 +1,45 @@
+<script setup lang="ts">
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+const stats = ref<{ worlds: { total: number; featured: number }, blocks: number }>();
+
+onMounted(async () => {
+    const response = await axios.get('https://api.wubbygame.com/statistics')
+    stats.value = response.data
+})
+</script>
+
 <template>
     <div class="text-center font-bold md:text-5xl">
         <h1>Statistics</h1>
     </div>
+    <Table class="mt-4 w-[600px] max-w-screen-md mx-auto">
+    <TableHeader>
+      <TableRow>
+        <TableHead class="text-left">Statistic</TableHead>
+        <TableHead class="text-right">Count</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="font-medium text-left">Total Worlds</TableCell>
+        <TableCell class="text-right">{{ stats?.worlds.total?.toLocaleString("en-US") }}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell class="font-medium text-left">Total Blocks</TableCell>
+        <TableCell class="text-right">{{ stats?.blocks?.toLocaleString("en-US") }}</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+    
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Hero from "@/components/Hero.vue";
+import Statistics from "@/components/Statistics.vue"
 </script>
 
 <template>
     <Hero />
+    <Statistics />
 </template>

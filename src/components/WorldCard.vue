95938b7e6ff7ec5a4c6bf422717ead5a2190adc5
choke <47597850+choke-dev@users.noreply.github.com>
2024-08-06 19:01:00 +0800

    make the world name text in bold

--- src/components/WorldCard.vue
+++ src/components/WorldCard.vue
@@ -2,7 +2,6 @@
 import { UUID } from 'crypto';
 import { defineProps } from 'vue';
 
-// Define the props interface for TypeScript
 interface World {
   id: number;
   activePlayers: string[] | { username: string, displayName: string, permission: number }[];
@@ -24,7 +23,6 @@ interface World {
   whitelistedPlayers: number[];
 }
 
-// Accept the `world` prop
 const props = defineProps<{
   world: World;
 }>();
@@ -40,15 +38,15 @@ const props = defineProps<{
     class="w-full h-60 object-cover mw-full"
     />
     
-    <div class="absolute bottom-4 left-4 bg-black bg-opacity-50 p-2 rounded text-white">
-      <p class="text-xs">{{ props.world.name }}</p>
-      <p class="text-xs">Creator: <a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator.id}/profile`">@{{ props.world.creator.name }}</a></p>
-      <p class="text-xs">World ID: {{ props.world.id }}</p>
+    <div class="absolute bottom-4 left-4 bg-black bg-opacity-50 p-2 rounded text-white text-left">
+      <p class="text-sm font-bold">{{ props.world.name }}</p>
+      <p class="text-xs"><a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator.id}/profile`">@{{ props.world.creator.name }}</a></p>
+      <p class="text-xs">{{ props.world.id }}</p>
     </div>
     
-    <div class="absolute bottom-4 right-4 bg-black bg-opacity-50 p-2 rounded text-white">
-      <p class="text-xs">Visits: {{ props.world.visits }} visits</p>
-      <p class="text-xs">Blocks: {{ props.world.blocks }} blocks</p>
+    <div class="absolute bottom-4 right-4 bg-black bg-opacity-50 p-2 rounded text-white text-right">
+      <p class="text-xs">{{ props.world.visits }} visits</p>
+      <p class="text-xs">{{ props.world.blocks }} blocks</p>
     </div>
   </div>
 </template>

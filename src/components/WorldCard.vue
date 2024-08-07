220929ff173d3ccb9bf538282d81287dfe8ecc32
choke <47597850+choke-dev@users.noreply.github.com>
2024-08-07 19:15:05 +0800

    feat: add support for multiple world thumbnaiils

--- src/components/WorldCard.vue
+++ src/components/WorldCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { UUID } from 'crypto';
 import { List, Lock, Trash2 } from 'lucide-vue-next';
-import { defineProps } from 'vue';
+import { defineProps, ref, computed } from 'vue';
 
 interface World {
   id: number;
@@ -27,45 +27,67 @@ interface World {
 const props = defineProps<{
   world: World;
 }>();
+
+const currentIndex = ref(0);
+const thumbnails = computed(() => Array.isArray(props.world.thumbnails) ? props.world.thumbnails : []);
+
+function updateIndex() {
+  if (thumbnails.value.length > 1) {
+    currentIndex.value = (currentIndex.value + 1) % thumbnails.value.length;
+  }
+}
+
+setInterval(updateIndex, 7000);
 </script>
 
 <template>
   <div class="relative rounded-lg shadow-md overflow-hidden min-w-[400px]">
-    <div
-      class="absolute inset-0 flex items-center justify-center"
-      v-if="[2, 3, 4].includes(props.world.privacyState)"
-    >
-      <div class="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center">
-        <div v-if="props.world.privacyState === 2" class="text-center">
-          <Lock class="h-16 w-16 text-white" />
-          Private
-        </div>
-        <div v-else-if="props.world.privacyState === 3" class="text-center">
-          <List class="h-16 w-16 text-white" />
-          Whitelist
-        </div>
-        <div v-else-if="props.world.privacyState === 4" class="text-center">
-          <Trash2 class="h-16 w-16 text-white" />
-          Deleted
-        </div>
+  
+  <div v-if="thumbnails.length > 0" class="relative w-full h-60 overflow-hidden">
+    <transition name="fade">
+      <img
+      :key="thumbnails[currentIndex]"
+      :src="`https://assetdelivery.roblox.com/v1/asset/?id=${thumbnails[currentIndex]}`"
+      class="absolute inset-0 w-full h-full object-cover"
+      />
+    </transition>
+  </div>
+  
+  <img
+  v-else
+  :src="`${Array.isArray(thumbnails) && thumbnails[0] 
+    ? `https://assetdelivery.roblox.com/v1/asset/?id=${thumbnails[0]}` 
+    : 'https://media.choke.dev/ShareX/2024/08/Roblox_AssetUnavailable.png'}`"
+  class="w-full h-60 object-cover"
+  />
+  
+  <div
+  class="absolute inset-0 flex items-center justify-center"
+  v-if="[2, 3, 4].includes(props.world.privacyState)"
+  >
+    <div class="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center">
+      <div v-if="props.world.privacyState === 2" class="text-center">
+        <Lock class="h-16 w-16 text-white" />
+        Private
+      </div>
+      <div v-else-if="props.world.privacyState === 3" class="text-center">
+        <List class="h-16 w-16 text-white" />
+        Whitelist
+      </div>
+      <div v-else-if="props.world.privacyState === 4" class="text-center">
+        <Trash2 class="h-16 w-16 text-white" />
+        Deleted
       </div>
     </div>
+  </div>
 
-    <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-50%"></div>
-
-    <img
-      :src="`${Array.isArray(props.world.thumbnails) && props.world.thumbnails[0] 
-      ? `https://assetdelivery.roblox.com/v1/asset/?id=${props.world.thumbnails[0]}` 
-      : 'https://media.choke.dev/ShareX/2024/08/Roblox_AssetUnavailable.png'}`"
-      class="w-full h-60 object-cover mw-full"
-    />
-
+  <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-50%"></div>
     <div class="absolute bottom-1 left-1 bg-black bg-opacity-0 p-2 rounded text-white text-left">
       <p class="text-base text-stroke font-bold font-figtree">{{ props.world.name }}</p>
       <p class="text-xs text-stroke">
-        <a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator.id}/profile`"
-          >@{{ props.world.creator.name }}</a
-        >
+        <a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator.id}/profile`">
+          @{{ props.world.creator.name }}
+        </a>
       </p>
       <p class="text-xs text-stroke">{{ props.world.id }}</p>
     </div>
@@ -80,9 +102,21 @@ const props = defineProps<{
 <style scoped>
 .text-stroke {
   text-shadow: 
-    -1px -1px 0 rgba(0, 0, 0, 0.2),
-     1px -1px 0 rgba(0, 0, 0, 0.2),
-    -1px  1px 0 rgba(0, 0, 0, 0.2),
-     1px  1px 0 rgba(0, 0, 0, 0.2);
+  -1px -1px 0 rgba(0, 0, 0, 0.2),
+  1px -1px 0 rgba(0, 0, 0, 0.2),
+  -1px  1px 0 rgba(0, 0, 0, 0.2),
+  1px  1px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* old image fade out */
+.fade-enter-from, .fade-leave-to {
+  transition: opacity 3s;
+  opacity: 0;
+}
+
+/* new image fade in */
+.fade-enter-to, .fade-leave-from {
+  transition: opacity 3s;
+  opacity: 1;
 }
 </style>

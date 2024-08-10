3e099c0bdd00f9afc61ca38902e46f77a868d31b
choke <47597850+choke-dev@users.noreply.github.com>
2024-08-10 11:33:50 +0800

    refactor: 404 page
    refactor: 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,17 @@
+<script setup lang="ts">
+import { FileQuestion } from 'lucide-vue-next';
+</script>
+
 <template>
   <div class="flex items-center justify-center h-full mt-24">
     <div class="text-center">
-      <h1 class="text-4xl font-bold">404</h1>
-      <p class="my-4">
+      <FileQuestion class="w-32 h-32 mx-auto" />
+      <h1 class="text-4xl my-4 font-bold">Page not found</h1>
+      <p>
         You seem lost wubbian, perhaps you should return to the 
-        <RouterLink to="/" class="text-blue-600 hover:underline">homepage</RouterLink>
-        ?
+        <RouterLink to="/" class="text-primary hover:underline">homepage</RouterLink>?
       </p>
     </div>
   </div>
 </template>
+

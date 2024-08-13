5cd5de13ab57c874e4d3140b9eee1cd89f81ce73
choke <47597850+choke-dev@users.noreply.github.com>
2024-08-13 20:43:42 +0800

    feat: searching by username

--- src/components/WorldCard.vue
+++ src/components/WorldCard.vue
@@ -2,16 +2,11 @@
 import { WubbyAPIWorldInfo } from 'global';
 import { Badge } from '@/components/ui/badge'
 import { List, Lock, Trash2 } from 'lucide-vue-next';
-import { defineProps, ref, computed } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps<{
-  world: Omit<WubbyAPIWorldInfo, 'creator'> & {
-    creator: {
-      id: number;
-      name: string;
-      displayName: string;
-    };
-  };
+  world: WubbyAPIWorldInfo,
+  creatorinfos: { id: number, name: string, displayName: string }[],
 }>();
 
 const currentIndex = ref(0);
@@ -22,6 +17,10 @@ function updateIndex() {
     currentIndex.value = (currentIndex.value + 1) % thumbnails.value.length;
   }
 }
+
+const creatorDetails = computed(() => {
+  return props.creatorinfos.find(creator => creator.id === props.world.creator)
+})
 
 setInterval(updateIndex, 7000);
 </script>
@@ -47,8 +46,8 @@ setInterval(updateIndex, 7000);
   <img
   v-else
   :src="`${thumbnails.length === 1
-    ? `https://assetdelivery.roblox.com/v1/asset/?id=${thumbnails[0]}` 
-    : 'https://media.choke.dev/ShareX/2024/08/Roblox_AssetUnavailable.png'}`"
+  ? `https://assetdelivery.roblox.com/v1/asset/?id=${thumbnails[0]}` 
+  : 'https://media.choke.dev/ShareX/2024/08/Roblox_AssetUnavailable.png'}`"
   onerror="this.onerror=null;this.src='https://media.choke.dev/ShareX/2024/08/Roblox_AssetUnavailable.png'"
   class="w-full h-60 object-cover"
   />
@@ -57,42 +56,42 @@ setInterval(updateIndex, 7000);
   class="absolute inset-0 flex items-center justify-center"
   v-if="[2, 3, 4].includes(props.world.privacyState)"
   >
-    <div class="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center">
-      <div v-if="props.world.privacyState === 2" class="text-center text-white">
-        <Lock class="h-16 w-16 text-white" />
-        Private
-      </div>
-      <div v-else-if="props.world.privacyState === 3" class="text-center">
-        <List class="h-16 w-16 text-white" />
-        Whitelist
-      </div>
-      <div v-else-if="props.world.privacyState === 4" class="text-center">
-        <Trash2 class="h-16 w-16 text-white" />
-        Deleted
-      </div>
+  <div class="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center">
+    <div v-if="props.world.privacyState === 2" class="text-center text-white">
+      <Lock class="h-16 w-16 text-white" />
+      Private
+    </div>
+    <div v-else-if="props.world.privacyState === 3" class="text-center">
+      <List class="h-16 w-16 text-white" />
+      Whitelist
+    </div>
+    <div v-else-if="props.world.privacyState === 4" class="text-center">
+      <Trash2 class="h-16 w-16 text-white" />
+      Deleted
     </div>
   </div>
+</div>
 
-  <div v-if="props.world.isFeatured" class="absolute left-0 top-0 bg-black bg-opacity-0 p-2 rounded text-white text-right">
-    <Badge class="border-transparent bg-[#fde047] bg-opacity-80 text-primary-foreground hover:bg-primary/80">Featured</Badge>
-  </div>
+<div v-if="props.world.isFeatured" class="absolute left-0 top-0 bg-black bg-opacity-0 p-2 rounded text-white text-right">
+  <Badge class="border-transparent bg-[#fde047] bg-opacity-80 text-primary-foreground hover:bg-primary/80">Featured</Badge>
+</div>
 
-  <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-50%"></div>
-    <div class="absolute bottom-1 left-1 bg-black bg-opacity-0 p-2 rounded text-white text-left">
-      <p class="text-base text-stroke font-bold font-figtree">{{ props.world.name }}</p>
-      <p class="text-xs text-stroke">
-        <a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator.id}/profile`">
-          @{{ props.world.creator.name }}
-        </a>
-      </p>
-      <p class="text-xs text-stroke">{{ props.world.id }}</p>
-    </div>
+<div class="absolute inset-0 bg-gradient-to-t from-black/50 to-50%"></div>
+<div class="absolute bottom-1 left-1 bg-black bg-opacity-0 p-2 rounded text-white text-left">
+  <p class="text-base text-stroke font-bold font-figtree">{{ props.world.name }}</p>
+  <p class="text-xs text-stroke">
+    <a target="_blank" :href="`https://www.roblox.com/users/${props.world.creator}/profile`">
+      @{{ creatorDetails ? creatorDetails.name : props.world.creator }}
+    </a>
+  </p>
+  <p class="text-xs text-stroke">{{ props.world.id }}</p>
+</div>
 
-    <div class="absolute bottom-1 right-1 bg-black bg-opacity-0 p-2 rounded text-white text-right">
-      <p class="text-xs text-stroke">{{ props.world.visits }} visits</p>
-      <p class="text-xs text-stroke">{{ props.world.blocks }} blocks</p>
-    </div>
-  </div>
+<div class="absolute bottom-1 right-1 bg-black bg-opacity-0 p-2 rounded text-white text-right">
+  <p class="text-xs text-stroke">{{ props.world.visits }} visits</p>
+  <p class="text-xs text-stroke">{{ props.world.blocks }} blocks</p>
+</div>
+</div>
 </template>
 
 <style scoped>
--- src/components/WorldList.vue
+++ src/components/WorldList.vue
@@ -9,78 +9,65 @@ import { WubbyAPIWorldInfo } from 'global';
 
 const loading = ref(false);
 const mode = useColorMode();
-const searchResults = ref();
 const skeletonCount = ref(0);
 
+const searchResults = ref();
+const creatorInfos = ref();
+
 const searchWorld = async (query: string) => {
-    if (loading?.value) return;
-    
-    // Validate the query
-    if (!query) {
-        searchResults.value = null;
-        return;
-    }
-    if (query.length < 3) {
-        searchResults.value = [false, "Search query must be at least 3 characters long"];
+    if (loading.value || !query || query.length < 3) {
+        searchResults.value = !query || query.length < 3 ? [false, "Search query must be at least 3 characters long"] : null;
         return;
     }
     
     loading.value = true;
     searchResults.value = null;
-    
+
     try {
-        
+        let response;
+        const isUserQuery = query.toLowerCase().startsWith('user:');
         const isNumericQuery = /^[0-9]+$/.test(query);
-        
-        if (isNumericQuery) {
+
+        if (isUserQuery) {
+            query = query.replace('user:', '');
+            const userResponse = await fetch(`https://users.roproxy.com/v1/users/search?keyword=${encodeURIComponent(query)}&limit=10`).then(res => res.json());
+
+            const matchedUser = userResponse.data.find((user: { name: string, previousUsernames: string[] }) => user.name.toLowerCase() === query.toLowerCase() || user.previousUsernames?.some((previousUsername) => previousUsername.toLowerCase() === query.toLowerCase()));            if (!matchedUser) {
+                searchResults.value = [false, "No user found"];
+                return;
+            }
+
+            response = await fetch(`https://api.wubbygame.com/v1/userworlds/${matchedUser.id}`).then(res => res.json());
+        } else if (isNumericQuery) {
             skeletonCount.value = 1;
-            const response = await fetch(`https://api.wubbygame.com/v1/worldinfo/${parseInt(query)}`).then(res => res.json());
-            searchResults.value = [true, [response]];
+            response = await fetch(`https://api.wubbygame.com/v1/worldinfo/${parseInt(query)}`).then(res => res.json());
         } else {
             skeletonCount.value = Math.floor(Math.random() * 12) + 1;
-            const response = await fetch(`https://api.wubbygame.com/v1/searchworld?query=${encodeURIComponent(query)}&limit=100`).then(res => res.json());
-            searchResults.value = response.length ? [true, response] : [false, "No results found"];
+            response = await fetch(`https://api.wubbygame.com/v1/searchworld?query=${encodeURIComponent(query)}&limit=100`).then(res => res.json());
         }
-        
-        if (searchResults.value[0]) {
-            const creatorUserIds: number[] = [];
-            searchResults.value[1].forEach((world: WubbyAPIWorldInfo) => {
-                creatorUserIds.push(world.creator);
-            });
-            
-            const response: { id: number; name: string; displayName: string }[] = await fetch('https://users.roproxy.com/v1/users', {
-                method: "POST",
+
+        searchResults.value = response.length ? [true, response] : [false, "No results found"];
+
+        const creatorIds = searchResults.value[1]?.map((world: WubbyAPIWorldInfo) => world.creator) || [];
+
+        if (creatorIds.length) {
+            const creatorResponse = await fetch(`https://users.roproxy.com/v1/users`, {
+                method: 'POST',
                 body: JSON.stringify({
-                    userIds: creatorUserIds,
+                    userIds: creatorIds,
                     excludeBannedUsers: false
                 })
-            }).then(res => res.json()).then(res => res.data);
-            
-            const userMap = new Map<number, { id: number; name: string; displayName: string }>();
-            response.forEach(user => {
-                userMap.set(user.id, user);
-            });
-            
-            searchResults.value[1].forEach((world: WubbyAPIWorldInfo) => {
-                const userResponse = userMap.get(world.creator);
-                if (userResponse) {
-                    // @ts-ignore
-                    world.creator = {
-                        id: userResponse.id,
-                        name: userResponse.name,
-                        displayName: userResponse.displayName
-                    };
-                }
-            });
+            }).then(res => res.json());
+
+            creatorInfos.value = creatorResponse.data;
         }
-        
-        
     } catch (err) {
+        console.warn(`Caught error: ${err}`);
         searchResults.value = [false, "An error occurred"];
     } finally {
         loading.value = false;
     }
-}
+};
 </script>
 
 <template>
@@ -97,7 +84,7 @@ const searchWorld = async (query: string) => {
         </div>
         <p class="text-center" v-if="!loading && searchResults && !searchResults[0]">{{ searchResults[1] }}</p>
         <div v-else-if="!loading && searchResults && searchResults[0]" class="grid grid-cols-1 lg:grid-cols-2 min-[1363px]:grid-cols-3 min-[1776px]:grid-cols-4 gap-4">
-            <WorldCard v-for="world in searchResults[1]" :key="world.id" :world="world" />
+            <WorldCard v-for="world in searchResults[1]" :key="world.id" :world="world" :creatorinfos="creatorInfos" />
         </div>
     </div>
 </template>

103b0be2854f0305a3f724092a892c80c2068a9a
integralblue <casualmorehere@gmail.com>
2025-07-08 21:02:18 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,8 +22,11 @@ export const IMAGE_CAROUSEL = { // FOR FUTURE REFERENCE, ONLY USE 1015x636 FOR T
         {
             id: 81930608228732,
             url: "https://files.catbox.moe/69k5jx.png"
+        },
+        {
+            id: 18170515127,
+            url: "https://files.catbox.moe/tnz5kh.png"
         }
-
     ]
 }
 

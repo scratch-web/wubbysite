0d8779c657d6650e69d635b106936f25c8d78a71
integralblue <casualmorehere@gmail.com>
2025-09-06 16:19:32 -0400

    Update Down.vue

--- src/views/Down.vue
+++ src/views/Down.vue
@@ -5,7 +5,7 @@ import { Gavel } from 'lucide-vue-next';
 <template>
   <div class="flex items-center justify-center h-full mt-24">
     <div class="text-center">
-      <FileQuestion class="w-32 h-32 mx-auto" />
+      <Gavel class="w-32 h-32 mx-auto" />
       <h1 class="text-4xl my-4 font-bold">Wubby Status</h1>
       <p>
        Wubby is down right now, check back later.

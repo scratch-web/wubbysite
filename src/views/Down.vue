d5b9b8691a26dba0f16ab97fe35256e1043e8c2f
integralblue <casualmorehere@gmail.com>
2025-09-07 22:31:10 -0400

    Update Down.vue

--- src/views/Down.vue
+++ src/views/Down.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { CloudAlert } from 'lucide-vue-next';
+import { ServerCrash } from 'lucide-vue-next';
 </script>
 
 <template>
   <div class="flex items-center justify-center h-full mt-24">
     <div class="text-center">
-      <CloudAlert class="w-32 h-32 mx-auto" />
+      <ServerCrash class="w-32 h-32 mx-auto" />
       <h1 class="text-4xl my-4 font-bold">Wubby Status</h1>
       <p>
       The API is currently down right now, please check back at a different time.<br>Update: If you want to access wubby, join the group.
